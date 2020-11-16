d359b6946672e024949f7361012548b04fc9858a
Irek <irek140@poczta.fm>
2020-11-16 11:21:12 +0100

    create logger

--- src/test/java/pl/project/clusteringspringboot/ClusteringSpringBootApplicationTests.java
+++ src/test/java/pl/project/clusteringspringboot/ClusteringSpringBootApplicationTests.java
@@ -3,8 +3,13 @@ package pl.project.clusteringspringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootTest
 class ClusteringSpringBootApplicationTests {
+
+    private final static Logger log = LogManager.getLogger("Main");
 
     @Test
     void contextLoads() {
